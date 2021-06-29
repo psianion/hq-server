7c480a82e00c3007fd5b5067282cbba2dd79bbb3
psianion <excerpt.snm@gmail.com>
2021-06-30 03:02:32 +0530

    Port Update

--- app.js
+++ app.js
@@ -74,4 +74,4 @@ app.get("/", isAuthorized, (req, res) => {
 });
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log("server is running..."));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
