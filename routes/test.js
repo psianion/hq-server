73d239b51de89d7fe1e435efc404e3750b72492a
psianion <excerpt.snm@gmail.com>
2022-08-09 12:12:24 +0530

    show only indian players

--- routes/test.js
+++ routes/test.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 router.get("/", function (req, res, next) {
   if (req.user) {
-    res.json("API is working properly");
+    res.json("API is working properly ig");
   } else {
     res.json("bro, login no?");
   }
