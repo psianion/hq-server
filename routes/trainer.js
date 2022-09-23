fe3a9636adc000cd7ae11ef71bef50047b91ca5d
psianion <excerpt.snm@gmail.com>
2022-09-23 14:30:07 +0530

    individual leaderboards

--- routes/trainer.js
+++ routes/trainer.js
@@ -24,4 +24,30 @@ router.get("/", async (req, res) => {
     });
 });
 
+router.get("/frontier", async (req, res) => {
+  User.find(
+    { "game.pokemongo.bf.s6.team": { $exists: true } },
+    {
+      "game.pokemongo.bf.s6.groupWins": 1,
+      "game.pokemongo.bf.s6.groupMatches": 1,
+      "game.pokemongo.bf.s6.knockoutWins": 1,
+      "game.pokemongo.bf.s6.knockoutMatches": 1,
+      "game.pokemongo.ign": 1,
+      "game.pokemongo.trainerTeam": 1,
+      nationality: 1,
+      "sprites.activeAvatar": 1,
+    }
+  )
+    .sort({
+      "game.pokemongo.bf.s6.groupWins": "descending",
+    })
+    .exec(function (err, model) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.json(model);
+      }
+    });
+});
+
 module.exports = router;
